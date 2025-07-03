425d846f7859d3891d297d035e9ec9222f59eeec
Elton Nike Casa <eltonnikecasa@gmail.com>
2025-07-03 11:47:10 -0300

    Update start-server.bat

--- start-server.bat
+++ start-server.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d "C:\theforest-server"
-start TheForestDedicatedServer.exe -batchmode -nographics -serverip SEU_IP_EXTERNO -serversteamport 8766 -servergameport 27015 -serverqueryport 27016 -servername NomeDoServidor -serverplayers 15 -difficulty Normal -inittype Continue -slot 1 -serverpassword SUA_SENHA
+start TheForestDedicatedServer.exe -batchmode -nographics -serverip SEU_IP_EXTERNO -serversteamport 8766 -servergameport 27015 -serverqueryport 27016 -servername NomeDoServidor -serverplayers 15 -difficulty Normal -initType Continue -slot 1 -serverpassword SUA_SENHA -serverpassword_admin SUA_SENHA_ADMIN -savepath "C:\theforest-server\ds" -serverAutoSaveInterval 5 -allowCheats off -enableVAC on
